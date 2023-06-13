2beaa5f31d0bdc197ed681a4a4f733bb442a03e1
Felix Notermans <felixn@live.nl>
2023-06-13 12:42:59 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Felix Notermans on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
