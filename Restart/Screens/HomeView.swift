201d5e0dc0c77ebf2d744f779bd92e614a994e8e
Felix Notermans <felixn@live.nl>
2023-06-19 12:03:31 +0200

    ios project demo

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,75 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Felix Notermans on 13/06/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            // MARK: - HEADER
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+                .offset(y: isAnimating ? 20 : -20)
+                .animation(
+                    Animation
+                        .easeInOut(duration: 4)
+                        .repeatForever()
+                        , value: isAnimating
+                    
+                )
+            }
+            
+            // MARK: - CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+                
+            // MARK: - FOOTER
+            
+            Spacer()
+            
+            
+            
+            Button(action: {
+                isOnboardingViewActive = true
+            }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+        }
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        })
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
